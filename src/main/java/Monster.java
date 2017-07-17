84cc022873b30eb8c6fa7f386217280b1ff4357f
Esvethlynna Pangelinan <esveth@aol.com>
2017-07-16 17:16:36 -0700

    include code for Exceptions

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -6,10 +6,22 @@ public class Monster {
   private String name;
   private int personId;
   private int id;
+  private int foodLevel;
+  private int sleepLevel;
+  private int playLevel;
+
+  public static final int MAX_FOOD_LEVEL =3;
+  public static final int MAX_SLEEP_LEVEL = 8;
+  public static final int MAX_PLAY_LEVEL = 12;
+  public static final int MIN_ALL_LEVELS = 0;
+
 
   public Monster(String name, int personId) {
     this.name = name;
     this.personId = personId;
+    playLevel = MAX_PLAY_LEVEL/2;
+    sleepLevel = MAX_SLEEP_LEVEL/2;
+    foodLevel = MAX_FOOD_LEVEL/2;
   }
 
   public String getName() {
@@ -22,6 +34,18 @@ public class Monster {
 
   public int getId() {
     return id;
+  }
+
+  public int getPlayLevel() {
+    return playLevel;
+  }
+
+  public int getSleepLevel() {
+    return sleepLevel;
+  }
+
+  public int getFoodLevel() {
+    return foodLevel;
   }
 
   @Override
@@ -61,4 +85,41 @@ public class Monster {
       return monster;
     }
   }
+
+  public boolean isAlive() {
+    if (foodLevel <= MIN_ALL_LEVELS || playLevel <= MIN_ALL_LEVELS || sleepLevel <= MIN_ALL_LEVELS) {
+      return false;
+    }
+    return true;
+  }
+
+  public void depleteLevels() {
+    playLevel--;
+    foodLevel--;
+    sleepLevel--;
+  }
+
+  public void play() {
+    if (playLevel >= MAX_PLAY_LEVEL) {
+      throw new UnsupportedOperationException("You cannot play with monster anymore!");
+    }
+    playLevel++;
+  }
+
+  public void sleep() {
+    if (sleepLevel >= MAX_SLEEP_LEVEL) {
+      throw new UnsupportedOperationException("You cannot make your monster sleep anymore!");
+    }
+    sleepLevel++;
+  }
+
+  public void feed() {
+    if (foodLevel >= MAX_FOOD_LEVEL) {
+      throw new UnsupportedOperationException("You cannot feed your monster anymore!");
+    }
+    foodLevel++;
+  }
+
+
+
 }
